0e97619b5a9f0287de194b55a312159c9acd974f
Antharya <antharyajalligampala@gmail.com>
2026-06-02 14:10:39 +0530

    fix(mobile): stabilize Jest module mapping for native mocks

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -5,11 +5,13 @@ module.exports = {
     'node_modules/(?!((react-native|@react-native|@react-navigation|@gorhom)/|\\.pnpm/(react-native|@react-native|@react-navigation|@gorhom)[^/]*))',
   ],
   moduleNameMapper: {
-    // Native packages that either lack a Jest-compatible build or are not
-    // installed as test dependencies. Mapped to local stubs so Jest bypasses
-    // node_modules resolution entirely.
-    'react-native-draggable-flatlist': '<rootDir>/__mocks__/react-native-draggable-flatlist.js',
-    'react-native-view-shot': '<rootDir>/__mocks__/react-native-view-shot.js',
-    'react-native-camera-kit': '<rootDir>/__mocks__/react-native-camera-kit.js',
-  },
+  '^react-native-draggable-flatlist$':
+    '<rootDir>/__mocks__/react-native-draggable-flatlist.js',
+
+  '^react-native-view-shot$':
+    '<rootDir>/__mocks__/react-native-view-shot.js',
+
+  '^react-native-camera-kit$':
+    '<rootDir>/__mocks__/react-native-camera-kit.js',
+},
 };
